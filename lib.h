c3fa94c1b8f77ea0b2d5e59dc479ce788696ed0e
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-04-12 11:48:54 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,15 +1,20 @@
 #ifndef Network_h
 #define Network_h
+#include <vector>
+#include <FirebaseESP32.h>
+#include <WiFi.h>
 
-class Network
-{
-  private:
-  
-  public:
-    Network();
-    void initWifi();
+#define FIREBASE_HOST "https://capsulasfinal-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "CpYYbimhc6DmdfR9Fv91NCvhWtTiEpb6B9Q5IVHU"
 
+const char *ssid = "Vivo-Internet-BF17";
+const char *password = "78814222";
 
-}
+typedef struct {
+  String Nome;
+  int Quantidade;
+} CafeTipo;
+
+std::vector<CafeTipo> jsonParse(String json);
 
 #endif
