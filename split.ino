5ad682ae4340ce41b7097c35755a896b716d55eb
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-04-13 11:36:36 -0300

    altas peripécias

--- split.ino
+++ split.ino
@@ -1,23 +1,5 @@
 #include "lib.h"
 
-//String jsonData = "[{\"Nome\":\"cafe preto\",\"quantidade\":100},{\"Nome\":\"cafe preto\",\"quantidade\":99},{\"Nome\":\"cafe preto\",\"quantidade\":98},{\"Nome\":\"cafe preto\",\"quantidade\":97},{\"Nome\":\"cafe preto\",\"quantidade\":96}]";
-
-
-
-//std::vector<CafeTipo> jsonParse(String json);
-
-//void setup() {
-//  Serial.begin(9600);
-//  while(!Serial) { }
-//  
-//  Serial.println(jsonData);
-//  std::vector<CafeTipo> cafes = jsonParse(jsonData);
-//  for (int i = 0; i < cafes.size(); i++)
-//  {
-//    Serial.print(cafes[i].Nome); Serial.print(" - "); Serial.println(cafes[i].Quantidade);
-//  }
-//}
-
 std::vector<CafeTipo> jsonParse(String json)
 {
   json.replace("[", "");
