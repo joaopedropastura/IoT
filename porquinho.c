66cd8d07ccf19e2e28d11b62ea2b797127636c20
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2023-06-16 16:44:21 -0300

    Update porquinho.c

--- porquinho.c
+++ porquinho.c
@@ -1,23 +1,40 @@
 #include <FirebaseESP32.h>
 #include <WiFi.h>
-
+#include <map>
+#include <Servo.h>
 
 #define FIREBASE_HOST "https://porquinho-e387d-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "KQsc6H51mKaBXsJt9dOLTLhu05MErDQAgj4jsy0A" 
 
+FirebaseData firebaseData;
+FirebaseJson json;
+Servo myservo;
 
 float amount;
 const char *ssid = "Hemer";
 const char *password = "zbqs9196";
-FirebaseData firebaseData;
-FirebaseJson json;
+
+std::map < int, float > coinValue = 
+{
+  { 25, 1 },
+  { 32, 0.5 },
+  { 33, 0.25 },
+  { 34, 0.1 },
+  { 35, 0.05 }
+};
+bool flag = false;
+int vet[5] = {34,35,32,33,25};
 
 void setup() {
+ 
+  Serial.begin(9600);
+  myservo.attach(26);
   pinMode(34, INPUT);
   pinMode(32, INPUT);
   pinMode(35, INPUT);
   pinMode(33, INPUT);
-  Serial.begin(9600);
+  pinMode(25, INPUT);
+
   
   WiFi.begin(ssid, password);
   
@@ -35,42 +52,45 @@ void setup() {
   Firebase.setwriteSizeLimit(firebaseData, "tiny");
   if(Firebase.getFloat(firebaseData, "Amount/networth"))
   {
-  if (firebaseData.dataTypeEnum() == fb_esp_rtdb_data_type_float) {
-      Serial.print(firebaseData.to<float>());
-      amount = firebaseData.to<float>();
+    if (firebaseData.dataTypeEnum() == fb_esp_rtdb_data_type_float) {
+        Serial.print(firebaseData.to<float>());
+        amount = firebaseData.to<float>();
+      }
+      else
+        Serial.println(firebaseData.errorReason());
     }
-    else{
-      Serial.println(firebaseData.errorReason());
-    }
+    myservo.write(147); 
+
   }
-Serial.print(amount);
+
+void AllowCoinPassage()
+{
+  myservo.write(60);
+  delay(1000);
+  myservo.write(147); 
 }
+
+float verify(int port)
+{
+  delay(500);
+    if(digitalRead(port))
+    {
+      AllowCoinPassage();
+      return coinValue[port];
+    }
+  Serial.println("Deu ruim");
+  return 0;
+}
+
 void loop() {
-  Serial.println(amount);
-  if(digitalRead(34))
-  {
-    amount += 1;
-    json.set("/networth", amount);
-    // Serial.println(amount);
-    delay(200);
-  }
-   if(digitalRead(35))
-  {
-    amount += 0.5;
-    json.set("/networth", amount);
-    // Serial.println(amount);
 
-    delay(200);
-  }
-  if(digitalRead(32))
+  for (int i = 0; i < 5; i++)
   {
-    amount += 0.25;
-    // Serial.println(amount);
-    json.set("/networth", amount);
-    delay(200);
+    if(digitalRead(vet[i]))
+    {
+      amount += verify(vet[i]);
+      json.set("/networth", amount);
+    }
   }
-    Firebase.updateNode(firebaseData, "/Amount", json);
-
-  if(digitalRead(33))
-    Serial.println("panela");
+  Firebase.updateNode(firebaseData, "/Amount", json);
 }
